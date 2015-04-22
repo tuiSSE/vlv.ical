6112d1e8aeb8fa0dd465a80971b67042bdfad6b2
elangenhan <mail@el42.de>
2015-04-22 09:12:34 +0200

    log all IDs of events

--- chrome_plugin/js/init.js
+++ chrome_plugin/js/init.js
@@ -1,8 +1,24 @@
 fixDivHeight();
+parse();
+
+/*
+ * Log the container that holds all relevant information
+ */
+console.log(document.getElementById("rechtespalte2"));
+
+
 
 /**
  * The VLV has a bad layout due to a single div containing a fixed height. This function fixes this by setting height to auto like it should be.
  */
 function fixDivHeight() {
     document.getElementById("rechtespalte2").style.height = "auto";
+}
+function parse() {
+    var list = document.getElementById("rechtespalte2").getElementsByTagName("div");
+    var i = 0;
+    for ( item in list ) {
+        console.log(document.getElementById("rechtespalte2").children[i].id);
+        i = i + 1;
+    }
 }
