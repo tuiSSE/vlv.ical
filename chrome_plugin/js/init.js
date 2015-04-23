abe6689ffdc154505c9e8bb74cdc87f25071c71a
elangenhan <mail@el42.de>
2015-04-23 12:49:53 +0200

    save information in JSON

--- chrome_plugin/js/init.js
+++ chrome_plugin/js/init.js
@@ -1,10 +1,10 @@
-var entryInfo = { 
-    entryPoint : "stupla_bold",
-    skip : 3
+var entryInfo = {
+    entryPoint: "stupla_bold",
+    skip: 3
 }
 
 fixDivHeight();
-parse(getSubjects(entryInfo));
+console.log(parse(getSubjects(entryInfo)));
 
 /*
  * Log the container that holds all relevant information
@@ -16,7 +16,7 @@ console.log(document.getElementById("rechtespalte2"));
 /*
  * The VLV has a bad layout due to a single div containing a fixed height. This function fixes this by setting height to auto like it should be.
  */
-function fixDivHeight() {
+function fixDivHeight() {
     document.getElementById("rechtespalte2").style.height = "auto";
 }
 
@@ -30,7 +30,7 @@ function getSubjects(entryInfo) {
     while (i < list.length) {
         subjects.push(list[i].parentNode);
         i = i + 1;
-    } 
+    }
     return subjects;
 }
 
@@ -41,17 +41,42 @@ function parse(rawSubjects) {
     var subjects = [];
     var titles = [];
     var speaker = [];
+
     for (item in rawSubjects) {
         var subject = rawSubjects[item];
         titles.push(getTitleOfSubject(subject));
         speaker.push(getSpeakerOfLecture(subject));
     }
-    
+
     var i = 0;
     while (i < rawSubjects.length) {
-        console.log("Name: " + titles[i]);
-        console.log("Vorlesende(r): " + speaker[i]);
-        console.log(" ");
+        var item = rawSubjects[i];
+        var data = {
+            name: "",
+            speaker: "",
+            lecture: {
+                dayOfWeek: "",
+                weekOfYear: "",
+                time: "",
+                location: "",
+                targetGroup: "",
+                lastUpdated: ""
+            },
+            lesson: {
+                dayOfWeek: "",
+                weekOfYear: "",
+                time: "",
+                location: "",
+                targetGroup: "",
+                lastUpdated: ""
+            }
+        };
+
+        data.name = getTitleOfSubject(item);
+        data.speaker = getSpeakerOfLecture(item);
+        
+
+        subjects.push(data);
         i = i + 1;
     }
     return subjects;
@@ -76,10 +101,10 @@ function getSpeakerOfLecture(subject) {
     return child.innerText.slice(12);
 }
 
-function getLecture() {
-
+function getLecture(subject) {
+    return subject.childNodes[5].childNodes[3];
 }
 
-function getLesson() {
-
+function getLesson(subject) {
+    return subject.childNodes[5].childNodes[4];
 }
