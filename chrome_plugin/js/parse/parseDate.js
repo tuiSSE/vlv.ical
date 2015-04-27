eddfc1c5ad327c2d4eca496c1aada897c8989d2c
elangenhan <mail@el42.de>
2015-04-27 21:02:28 +0200

    more restructuring

---
+++ chrome_plugin/js/parse/parseDate.js
@@ -0,0 +1,69 @@
+function getDate(w, y, day) {
+    var simple = new Date(y, 0, 1 + (w - 1) * 7);
+    var date = simple;
+    var dow = simple.getDay();
+    var weekStart = simple;
+    if (dow <= 4)
+        weekStart.setDate(simple.getDate() - simple.getDay() + 1);
+    else
+        weekStart.setDate(simple.getDate() + 8 - simple.getDay());
+
+    switch(day) {
+      case 'Montag':
+        break;
+
+      case 'Dienstag':
+        date.setDate(weekStart.getDate() + 1);
+        break;
+
+      case 'Mittwoch':
+        date.setDate(weekStart.getDate() + 2);
+        break;
+
+      case 'Donnerstag':
+        date.setDate(weekStart.getDate() + 3);
+        break;
+
+      case 'Freitag':
+        date.setDate(weekStart.getDate() + 4);
+        break;
+
+      case 'Samstag':
+        date.setDate(weekStart.getDate() + 5);
+        break;
+
+      case 'Sonntag':
+        date.setDate(weekStart.getDate() + 6);
+        break;
+        }
+
+    return date;
+}
+
+
+// TODO: get week and year
+function parseTime(raw, day) {
+  raw = raw.split(" ");
+  var hours = [];
+  hours[0] = raw[0].split('.');
+  hours[1] = raw[2].split('.');
+
+  var time =[];
+  try {
+    var dt = getDate(18, 2015, day);
+  } catch(e) { console.log(e); }
+
+  var year = dt.getFullYear().toString();
+
+  var month = dt.getMonth() + 1;
+  if (month < 10) {
+    month = '0' + month.toString();
+  }
+
+  var date = dt.getDate().toString();
+
+  time[0] = new String(year + month + date + 'T' + hours[0][0] + hours[0][1] + '00');
+  time[1] = new String(year + month + date + 'T' + hours[1][0] + hours[1][1] + '00');
+
+  return time;
+}
