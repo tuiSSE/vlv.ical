60be6dc1037d55511a61af5fa1f92ce07d6ca74a
elangenhan <mail@el42.de>
2015-05-17 17:04:41 +0200

    fix previous commit

---
+++ chrome_plugin/js/inject/selection.js
@@ -0,0 +1,37 @@
+function updateSelection() {
+     var selection = loadObjects('selection');
+     try {
+            for (var i = 0; i < subjects.length; i++){
+               try {
+                      if (containsObject(subjects[i], selection)) {
+                              subjects[i].style.background = '#BEE8BA';
+                      } else {
+                              subjects[i].style.background = 'white';
+                      }
+               } catch (e) {
+                     console.log(e);
+               }
+            }
+      } catch (e) {
+            console.log(e);
+        }
+      try {
+            updateSelectionBox();
+      } catch (e) {
+            console.log(e);
+      }
+}
+
+function addToCart(object) {
+      var selection = loadObjects('selection');
+      selection.push(object);
+      saveObjects('selection', selection);
+      updateSelection();
+}
+
+function removeFromCart(object) {
+      var selection = loadObjects('selection');
+      selection = removeFromList(selection, getObjectIndex(object, selection));
+      saveObjects('selection', selection);
+      updateSelection();
+}
---
+++ chrome_plugin/js/inject/selectionBox.js
@@ -0,0 +1,35 @@
+function updateSelectionBox() {
+	var selection = loadObjects('selection');
+	var box = $('#selectionBox')[0];
+	box.innerHTML = '';
+	var clear = document.createElement('input');
+	clear.className = 'clearButton';
+	clear.type = 'button';
+	clear.value = 'clear';
+	clear.onclick = clearSelectionBox;
+	box.appendChild(clear);
+	
+	for (var i = 0; i < selection.length; i++) {
+		var name = getNameOfLecture(selection[i]);
+		var element = document.createElement('input');
+		element.className = 'selectionBoxItem';
+		element.type = 'button';
+		element.value = name;
+		box.appendChild(element);
+	}
+}
+
+function injectDiv() {
+  var div = $('<div id="emptyBox"><br><br><br><br></div>');
+  div.insertBefore(document.body.childNodes[0]);
+  
+  var box = $('<div id="selectionBox"></div>')
+  box.insertBefore(document.body.childNodes[0]);
+}
+
+function clearSelectionBox() {
+	var selection = loadObjects('selection');
+	selection = [];
+	saveObjects('selection', selection);
+	updateSelection();
+}
