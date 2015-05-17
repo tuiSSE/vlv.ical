8fdfdbe2d38d8b519e5002348c75e6b04f6c3386
elangenhan <mail@el42.de>
2015-05-17 17:58:52 +0200

    increase id size

--- chrome_plugin/js/inject/identifier.js
+++ chrome_plugin/js/inject/identifier.js
@@ -7,7 +7,7 @@ function makeid()
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i < 5; i++ )
+    for( var i=0; i < 10; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
