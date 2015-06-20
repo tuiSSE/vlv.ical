b51405a68410aa4e0f5d2cb7087007dc080f7497
Norris Sam Osarenkhoe <nrrsso@gmail.com>
2015-06-21 00:58:48 +0200

    close #90, close #87, close #70

--- chrome_plugin/js/inject/selectionBox.js
+++ chrome_plugin/js/inject/selectionBox.js
@@ -40,13 +40,178 @@ function openEditDialog(id) {
                           var id = $('#editSelectId')[0].value;
                           var index = $('#editSelectIndex')[0].value;
                           var data = load(id);
+                          var begin = data.objects[index].begin;
 
-                          if(data.objects[i].begin.length > 1) {
-                            console.log('Houston, wir haben ein Problem!');
+                          if(Array.isArray(begin)) {
+                            openEditDialogDetailMulti(id, index);
                           } else {
                             openEditDialogDetail(id, index);
                           }
                         }
+                    }
+                },
+                keyboard: false
+            }
+        );
+}
+
+function openEditDialogDetailMulti(id, index) {
+  var data = load(id);
+  var i = index;
+
+  var date = [],
+      begin = [],
+      end = [];
+
+  var length = data.objects[i].begin.length;
+  for (var j = 0; j < length; j++) {
+    date.push(data.objects[i].begin[j].slice(0, 4) + '-' + data.objects[i].begin[j].slice(4, 6) + '-' + data.objects[i].begin[j].slice(6, 8));
+    begin.push(data.objects[i].begin[j].slice(9, 11) + ':' + data.objects[i].begin[j].slice(11, 13));
+    end.push(data.objects[i].end[j].slice(9, 11) + ':' + data.objects[i].end[j].slice(11, 13));
+  }
+  
+  // assign the values of the object
+  $('#editMultiId').val( id );
+  $('#multiName').val( data.name );
+  $('#multiLocation').val( data.objects[i].location );
+  $('#multiComment').val( data.objects[i].comment );
+
+  var origForm = $('#editFormMulti'); // Copy of the Form before Edit
+  var clonedForm = null; // Holds the edited Form
+
+  for (var j = 0; j < length; j++) {
+    var addedInputs = '<div class="form-group">' +
+                      '<label for="date'+ j +'">Datum</label>' +
+                      '<input id="multiDate'+ j +'" name="date'+ j +'" type="date" class="form-control input-md">' +
+                      '</div>' +
+                      '<div class="form-group">' +
+                      '<label for="begin'+ j +'">Beginn</label>' +
+                      '<input id="multiBeginTime'+ j +'" name="begin'+ j +'" type="time" class="form-control input-md">' +
+                      '</div>' +
+                      '<div class="form-group">' +
+                      '<label for="end'+ j +'">Ende</label>' +
+                      '<input id="multiEndTime'+ j +'" name="end'+ j +'" type="time" class="form-control input-md">' +
+                      '</div>';
+
+    $('.form-group #multiComment').parent().after(addedInputs);
+
+    $('#multiDate' + j).val( date[j] );
+    $('#multiBeginTime' + j).val( begin[j] );
+    $('#multiEndTime' + j).val( end[j] );
+  }
+  $('#multiRepeat').val( data.objects[i].weekly );
+  
+
+  bootbox.dialog({
+                title: data.origName,
+                message: $('#editFormMulti'),
+                backdrop: true,
+                closeButton: false,
+                buttons: {
+                    success: {
+                        label: "Save",
+                        className: "btn-success",
+                        callback: function () {
+                          var i = index;
+                          try {
+                            var id = $('#editMultiId')[0].value;
+                            var data = load(id);
+                          } catch (e) {
+                            $('#formArea div:nth-child(2)').prepend(origForm);
+                            toastr.error(e, 'Error');
+                          }
+                          
+                          try {
+                            data.name = isEmpty($('#multiName')[0].value);
+                            data.location = isEmpty($('#multiLocation')[0].value);
+                            data.comment = isEmpty($('#multiComment')[0].value);
+                            
+                            var date = [],
+                                beginTime = [],
+                                endTime = [];
+
+                            for (var j = 0; j < length; j++) {
+                              date.push(isEmpty($('#multiDate' + j)[0].value));
+                              date[j] = date[j].split("-").join('');
+
+                              beginTime.push(isEmpty($('#multiBeginTime' + j)[0].value));
+                              beginTime[j] = beginTime[j].slice(0, 2) + beginTime[j].slice(3, 5) + '00';
+
+                              endTime.push(isEmpty($('#multiEndTime' + j)[0].value));
+                              endTime[j] = endTime[j].slice(0, 2) + endTime[j].slice(3, 5) + '00';
+
+                              data.objects[i].begin[j] = date[j] + 'T' + beginTime[j];
+                              data.objects[i].end[j] = date[j] + 'T' + endTime[j];
+                            }
+                            
+                            clonedForm = $('#editFormMulti');
+                          } catch(e) {
+                            console.error(e.name+": "+e.message);
+                            toastr.error(e, 'Error');
+                            return false;   // Prevent the Modal from closing
+                          }
+                          
+                          try {
+                              for (var j = 0; j < length; j++) {
+                                $('[for="date'+ j +'"]').parent().remove();
+                                $('[name="date'+ j +'"]').remove();
+                                $('[for="date'+ j +'"]').remove();
+
+                                $('[for="begin'+ j +'"]').parent().remove();
+                                $('[name="begin'+ j +'"]').remove();
+                                $('[for="begin'+ j +'"]').remove();
+
+                                $('[for="end'+ j +'"]').parent().remove();
+                                $('[name="end'+ j +'"]').remove();
+                                $('[for="end'+ j +'"]').remove();
+                              }
+                              $('#formArea div:nth-child(2)').prepend(clonedForm);
+                              save(id, data);
+                          } catch(e) {
+                            for (var j = 0; j < length; j++) {
+                                $('[for="date'+ j +'"]').parent().remove();
+                                $('[name="date'+ j +'"]').remove();
+                                $('[for="date'+ j +'"]').remove();
+
+                                $('[for="begin'+ j +'"]').parent().remove();
+                                $('[name="begin'+ j +'"]').remove();
+                                $('[for="begin'+ j +'"]').remove();
+
+                                $('[for="end'+ j +'"]').parent().remove();
+                                $('[name="end'+ j +'"]').remove();
+                                $('[for="end'+ j +'"]').remove();
+                              }
+                            $('#formArea div:nth-child(2)').prepend(origForm);
+                            toastr.error(e, 'Error');
+                          }
+
+                          updateSelectionBox();
+                          
+                        }
+                    },
+                    cancel: {
+                      label: "Cancel",
+                      className: "btn-danger",
+                      callback: function(){
+                        try {
+                          for (var j = 0; j < length; j++) {
+                                $('[for="date'+ j +'"]').parent().remove();
+                                $('[name="date'+ j +'"]').remove();
+                                $('[for="date'+ j +'"]').remove();
+
+                                $('[for="begin'+ j +'"]').parent().remove();
+                                $('[name="begin'+ j +'"]').remove();
+                                $('[for="begin'+ j +'"]').remove();
+
+                                $('[for="end'+ j +'"]').parent().remove();
+                                $('[name="end'+ j +'"]').remove();
+                                $('[for="end'+ j +'"]').remove();
+                              }
+                          $('#formArea div:nth-child(2)').prepend(origForm);
+                        } catch(e) {
+                          console.log(e);
+                        }
+                      }
                     }
                 },
                 keyboard: false
@@ -88,7 +253,7 @@ function openEditDialogDetail(id, index) {
                             var id = $('#editId')[0].value;
                             var data = load(id);
                           } catch (e) {
-                            $('#formArea').prepend(origForm);
+                            $('#formArea div:nth-child(1)').prepend(origForm);
                             toastr.error(e, 'Error');
                           }
                           
@@ -120,10 +285,10 @@ function openEditDialogDetail(id, index) {
                           }
                           
                           try {
-                              $('#formArea').prepend(clonedForm);
+                              $('#formArea div:nth-child(1)').prepend(clonedForm);
                               save(id, data);
                           } catch(e) {
-                            $('#formArea').prepend(origForm);
+                            $('#formArea div:nth-child(1)').prepend(origForm);
                             toastr.error(e, 'Error');
                           }
 
@@ -136,7 +301,7 @@ function openEditDialogDetail(id, index) {
                       className: "btn-danger",
                       callback: function(){
                         try {
-                          $('#formArea').prepend(origForm);
+                          $('#formArea div:nth-child(1)').prepend(origForm);
                         } catch(e) {
                           console.log(e);
                         }
