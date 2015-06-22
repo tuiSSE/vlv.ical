dd31294b790e04cba71fb497a9a8703544772ea8
elangenhan <mail@el42.de>
2015-06-22 11:26:00 +0200

    close #98

--- chrome_plugin/js/inject/borders.js
+++ chrome_plugin/js/inject/borders.js
@@ -1,11 +1,24 @@
 function injectBorders() {
-    var subjects = getRootElement().getElementsByTagName('div');
     var now = new Date();
-    var changed = [];
     now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
     for (var i = 0; i < subjects.length; i++) {
         var object = subjects[i];
+
+        var rawLastUpdated = getLastUpdated(object);
+        var year = rawLastUpdated.slice(6);
+        var month = rawLastUpdated.slice(3, (rawLastUpdated.length - 3)) - 1;
+        var day = rawLastUpdated.slice(0, rawLastUpdated.length - 6);
+        var lastUpdated = new Date('20' + year, month, day);
+
+        var twoWeeks = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 14);
         object.style.border = '2px solid #435779';
+
+        if (lastUpdated > twoWeeks) {
+        	var obj = object.childNodes[5].childNodes[3].childNodes[0].childNodes[13];
+            obj.style.background = '#ea6a68';
+            obj.style.color = 'white';
+            obj.style.padding = "2px";
+        }
     }
 }
